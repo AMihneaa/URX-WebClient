a114f51c214263835e6de46ef485b4db01765306
unknown <asmihnea5@gmail.com>
2024-07-23 15:46:13 +0300

    Robot and Camera

--- RobotWeb/Test/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-
-# Create your views here.
-def test_view(request):
-    data = {'message': 'Buna Miruna'}
-    return JsonResponse(data)
